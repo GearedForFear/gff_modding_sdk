a3d2b76c0d9ba1dfe528f04ccb9dcaece424349a
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2022-10-28 21:28:46 +0200

    Update turbulence.gd

--- base_game/scripts/vehicles/turbulence.gd
+++ base_game/scripts/vehicles/turbulence.gd
@@ -1,9 +1,18 @@
 extends AmmoVehicle
 
 
-var can_shoot: bool = true
+const Grenade: PackedScene \
+		= preload("res://scenes/weapon_components/grenade.tscn")
 
-onready var gun_timer: Timer = get_node("../GunTimer")
+export var damage: float = 20.0
+export var reward: int = 20
+export var burn: float = 0.1
+export var ammo_cost: float = 10.0
+
+var can_shoot_middle: bool = true
+var can_shoot_left: bool = true
+var can_shoot_right: bool = true
+var glide: bool = false
 
 
 func _ready():
@@ -26,8 +35,56 @@ func _physics_process(_delta):
 		if controls == null:
 			pass
 		else:
-			pass
+			if can_shoot_middle and ammo >= ammo_cost \
+					and Input.is_action_pressed(controls.weapon_front):
+				ammo -= ammo_cost
+				can_shoot_middle = false
+				get_node("../MiddleTimer").start()
+				$ShotPositionMiddle.add_child(instantiate_grenade())
+			
+			if can_shoot_left and ammo >= ammo_cost \
+					and Input.is_action_pressed(controls.weapon_left):
+				ammo -= ammo_cost
+				can_shoot_left = false
+				get_node("../LeftTimer").start()
+				$ShotPositionLeft.add_child(instantiate_grenade())
+			
+			if can_shoot_right and ammo >= ammo_cost \
+					and Input.is_action_pressed(controls.weapon_right):
+				ammo -= ammo_cost
+				can_shoot_right = false
+				get_node("../RightTimer").start()
+				$ShotPositionRight.add_child(instantiate_grenade())
+			
+			if Input.is_action_just_pressed(controls.weapon_back):
+				if glide:
+					get_node("../AnimationPlayer").play("out_in")
+				else:
+					get_node("../AnimationPlayer").play("in_out")
+				glide = !glide
+	
+	if glide:
+		apply_central_impulse(transform.basis.y * clamp((linear_velocity \
+				* Vector3(1, 0, 1)).length() - 20, 0, 10))
 
 
-func _on_GunTimer_timeout():
-	can_shoot = true
+func instantiate_grenade() -> Area:
+	var new_grenade: Area = Grenade.instance()
+	new_grenade.damage = damage
+	new_grenade.reward = reward
+	new_grenade.burn = burn
+	new_grenade.shooter = self
+	new_grenade.deletion_manager = deletion_manager
+	return new_grenade
+
+
+func _on_MiddleTimer_timeout():
+	can_shoot_middle = true
+
+
+func _on_LeftTimer_timeout():
+	can_shoot_left = true
+
+
+func _on_RightTimer_timeout():
+	can_shoot_right = true
