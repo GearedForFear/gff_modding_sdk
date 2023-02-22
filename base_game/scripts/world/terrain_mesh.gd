c66438433b6d0e7050676359111e7aaae0358cb0
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-02-23 00:40:09 +0100

    Update terrain_mesh.gd

--- base_game/scripts/world/terrain_mesh.gd
+++ base_game/scripts/world/terrain_mesh.gd
@@ -2,5 +2,5 @@ extends MeshInstance
 
 
 func _ready():
-	if get_node("/root/RootControl/SettingsManager").shadow_amount == 0:
+	if get_node("/root/RootControl/SettingsManager").shadow_casters == 0:
 		cast_shadow = GeometryInstance.SHADOW_CASTING_SETTING_OFF
