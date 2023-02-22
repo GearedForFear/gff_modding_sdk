c9b7c0555d91d93dc94cb87b7afa20ffbf2f5eb9
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-02-23 00:32:09 +0100

    Add files via upload

---
+++ base_game/scripts/custom_resources/vehicle_data.gd
@@ -0,0 +1,12 @@
+class_name VehicleData
+extends Object
+
+
+var scene_resource: String
+var controls: PlayerControls
+var spawn: int
+var score:int
+
+
+static func sort_ascending(a: VehicleData, b: VehicleData) -> bool:
+	return a.spawn < b.spawn
