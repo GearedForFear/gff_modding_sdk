eccc8123aef19fee5e4dabeacc68c13b259e08bf
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-02-23 00:39:47 +0100

    Update default_shadow_mesh.gd

--- base_game/scripts/world/default_shadow_mesh.gd
+++ base_game/scripts/world/default_shadow_mesh.gd
@@ -2,5 +2,5 @@ extends MeshInstance
 
 
 func _ready():
-	if get_node("/root/RootControl/SettingsManager").shadow_amount < 3:
+	if get_node("/root/RootControl/SettingsManager").shadow_casters < 3:
 		cast_shadow = GeometryInstance.SHADOW_CASTING_SETTING_OFF
