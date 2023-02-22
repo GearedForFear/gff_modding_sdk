2c80d3ac5d0046478b20de19a409731c926cab46
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-02-23 00:38:26 +0100

    Update ultimate_shadow_mesh.gd

--- base_game/scripts/weapon_compontents/ultimate_shadow_mesh.gd
+++ base_game/scripts/weapon_compontents/ultimate_shadow_mesh.gd
@@ -2,5 +2,5 @@ extends MeshInstance
 
 
 func _ready():
-	if get_node("/root/RootControl/SettingsManager").shadow_amount == 5:
+	if get_node("/root/RootControl/SettingsManager").shadow_casters == 5:
 		cast_shadow = GeometryInstance.SHADOW_CASTING_SETTING_ON
