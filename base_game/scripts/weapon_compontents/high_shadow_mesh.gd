96749f03a1e096701ce67dd851d2700b9b4f6c4b
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-02-23 00:37:33 +0100

    Update high_shadow_mesh.gd

--- base_game/scripts/weapon_compontents/high_shadow_mesh.gd
+++ base_game/scripts/weapon_compontents/high_shadow_mesh.gd
@@ -2,5 +2,5 @@ extends MeshInstance
 
 
 func _ready():
-	if get_node("/root/RootControl/SettingsManager").shadow_amount >= 4:
+	if get_node("/root/RootControl/SettingsManager").shadow_casters >= 4:
 		cast_shadow = GeometryInstance.SHADOW_CASTING_SETTING_ON
