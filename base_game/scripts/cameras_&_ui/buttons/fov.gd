a6b03d7930fdc621ebf7e17b4efa474ce7ce97a3
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:28:31 +0200

    Update fov.gd

--- base_game/scripts/cameras_&_ui/buttons/fov.gd
+++ base_game/scripts/cameras_&_ui/buttons/fov.gd
@@ -3,10 +3,14 @@ extends Button
 
 export var field_of_view: int = 75
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").field_of_view \
-			= field_of_view
-	get_node("/root/RootControl").switch_buttons(get_node("../.."), \
+	root_control.get_node("SettingsManager").field_of_view = field_of_view
+	config.set_value("graphics", "field_of_view", field_of_view)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_node("../.."), \
 			get_node("../../../GraphicsButtons/FieldOfView"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
