f953130c7cb9d7a11142952bf2963829a04e583c
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:14:31 +0200

    Update chainsaw.gd

--- base_game/scripts/weapon_compontents/chainsaw.gd
+++ base_game/scripts/weapon_compontents/chainsaw.gd
@@ -1,10 +1,7 @@
 extends Area
 
 
-const Money: PackedScene = preload("res://scenes/collectables/money.tscn")
-const Sparks: PackedScene = preload("res://scenes/destruction/sparks.tscn")
-
-export var speed: float = 0.5
+const Explosion: PackedScene = preload("res://scenes/destruction/explosion.tscn")
 
 var damage: float
 var reward: int
@@ -23,40 +20,37 @@ func _ready():
 
 
 func _physics_process(_delta):
-	translation += transform.basis.z * speed
-	transform.basis.z = transform.basis.z.linear_interpolate(Vector3.DOWN, 0.01)
-	interpolation_weight *= 1.1
+	translation += transform.basis.z * 0.7
+	global_transform = global_transform.interpolate_with(global_transform.\
+			looking_at(global_translation - global_transform.basis.z \
+			+ Vector3.UP, Vector3.UP), interpolation_weight)
+	interpolation_weight *= 1.02
 
 
 func _on_Lifetime_timeout():
 	set_process(false)
+	set_physics_process(false)
 	hide()
 	deletion_manager.to_be_deleted.append(self)
 
 
 func _on_Area_body_entered(body):
 	if body != shooter:
-		if body.is_in_group("combat_vehicle"):
+		if true:
 			$RayCast.force_raycast_update()
-			var payout: int = body.damage(damage, reward, burn, shooter)
-			if payout > 0:
-				var new_money: Area = Money.instance()
-				new_money.shooter = shooter
-				new_money.reward = payout
-				new_money.deletion_manager = deletion_manager
-				body.add_child(new_money)
-				new_money.global_transform.origin = \
-						$RayCast.get_collision_point()
-				if $RayCast.get_collision_point() == Vector3.ZERO:
-					new_money.global_transform.origin = global_transform.origin
-			var new_sparks: MeshInstance = Sparks.instance()
-			new_sparks.deletion_manager = deletion_manager
-			body.add_child(new_sparks)
-			new_sparks.global_transform = global_transform
-			new_sparks.rotation.z = randi() / TAU
-			new_sparks.global_transform.origin = $RayCast.get_collision_point()
+			var new_explosion: Area = Explosion.instance()
+			new_explosion.damage = damage
+			new_explosion.reward = reward
+			new_explosion.burn = burn
+			new_explosion.shooter = shooter
+			new_explosion.deletion_manager = deletion_manager
+			body.add_child(new_explosion)
+			new_explosion.global_transform = global_transform
+			new_explosion.global_transform.origin \
+					= $RayCast.get_collision_point()
 			if $RayCast.get_collision_point() == Vector3.ZERO:
-				new_sparks.global_transform.origin = global_transform.origin
+				new_explosion.global_transform.origin = global_transform.origin
 		set_process(false)
+		set_physics_process(false)
 		hide()
 		deletion_manager.to_be_deleted.append(self)
