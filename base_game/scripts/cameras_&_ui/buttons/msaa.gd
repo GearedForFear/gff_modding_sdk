300fa3bbe1b0ab7bc37cc01f1e50c0a74a630281
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:28:53 +0200

    Update msaa.gd

--- base_game/scripts/cameras_&_ui/buttons/msaa.gd
+++ base_game/scripts/cameras_&_ui/buttons/msaa.gd
@@ -3,9 +3,14 @@ extends Button
 
 export var msaa: int = 0
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").msaa = msaa
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	root_control.get_node("SettingsManager").msaa = msaa
+	config.set_value("graphics", "msaa", msaa)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/MSAA"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
