665875c69b8dbc90f0ca83cc27126437993f82de
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:31:35 +0200

    Update shadow_casters.gd

--- base_game/scripts/cameras_&_ui/buttons/shadow_casters.gd
+++ base_game/scripts/cameras_&_ui/buttons/shadow_casters.gd
@@ -1,12 +1,17 @@
 extends Button
 
 
-export var shadow_casters: int = 0
+export var shadow_casters: int = 3
+
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
 
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").shadow_casters \
+	root_control.get_node("SettingsManager").shadow_casters \
 			= shadow_casters
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	config.set_value("graphics", "shadow_casters", shadow_casters)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/ShadowCasters"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
