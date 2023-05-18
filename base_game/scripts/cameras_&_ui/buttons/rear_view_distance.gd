86f8da25cd6677b06e83621177a0d02e0d4fd669
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:29:39 +0200

    Update rear_view_distance.gd

--- base_game/scripts/cameras_&_ui/buttons/rear_view_distance.gd
+++ base_game/scripts/cameras_&_ui/buttons/rear_view_distance.gd
@@ -1,12 +1,16 @@
 extends Button
 
 
-export var distance: int = 200.0
+export var distance: float = 200.0
+
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
 
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").rear_view_distance \
-			= distance
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	root_control.get_node("SettingsManager").rear_view_distance = distance
+	config.set_value("graphics", "rear_view_distance", distance)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/RearViewDistance"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
