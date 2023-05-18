7ac57854c72aceb7412921ef0b580a7ae2dfe9c4
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:31:58 +0200

    Update shadow_distance.gd

--- base_game/scripts/cameras_&_ui/buttons/shadow_distance.gd
+++ base_game/scripts/cameras_&_ui/buttons/shadow_distance.gd
@@ -1,15 +1,20 @@
 extends Button
 
 
-export var shadow_distance: int = 200
+export var shadow_distance: float = 200.0
 export var shadow_quality: int = 4096
+
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
 
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").shadow_distance \
-			= shadow_distance
+	root_control.get_node("SettingsManager").shadow_distance = shadow_distance
 	ProjectSettings.set_setting("rendering/quality/directional_shadow/size", \
 			shadow_quality)
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	config.set_value("graphics", "shadow_distance", shadow_distance)
+	config.set_value("graphics", "shadow_quality", shadow_quality)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/ShadowDistance"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
