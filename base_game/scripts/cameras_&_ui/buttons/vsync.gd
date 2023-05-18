36694b5f2b70f67c69ee811187c8e9cbe6292444
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:34:41 +0200

    Update vsync.gd

--- base_game/scripts/cameras_&_ui/buttons/vsync.gd
+++ base_game/scripts/cameras_&_ui/buttons/vsync.gd
@@ -1,6 +1,12 @@
 extends Button
 
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
+
 func _pressed():
 	OS.vsync_enabled = !OS.vsync_enabled
+	config.set_value("graphics", "vsync", OS.vsync_enabled)
+	config.save("user://config.cfg")
 	get_node("/root/RootControl/ButtonPressAudio").play()
