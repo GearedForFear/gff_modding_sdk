978025ed350b80ab2b63c9d2e2088c5b10eda0a7
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:33:58 +0200

    Update volume.gd

--- base_game/scripts/cameras_&_ui/buttons/volume.gd
+++ base_game/scripts/cameras_&_ui/buttons/volume.gd
@@ -4,13 +4,19 @@ extends Button
 export var bus_idx: int = 1
 export var volume_linear : float = 0.0
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
 
 func _pressed():
 	AudioServer.set_bus_volume_db(bus_idx, linear2db(volume_linear))
 	if bus_idx == 1:
-		get_node("/root/RootControl").switch_buttons(get_node("../.."), \
+		config.set_value("audio", "sound", volume_linear)
+		root_control.switch_buttons(get_node("../.."), \
 				get_node("../../../SoundButtons/EffectsVolume"))
 	else:
-		get_node("/root/RootControl").switch_buttons(get_node("../.."), \
+		config.set_value("audio", "music", volume_linear)
+		root_control.switch_buttons(get_node("../.."), \
 				get_node("../../../SoundButtons/MusicVolume"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	config.save("user://config.cfg")
+	root_control.get_node("ButtonPressAudio").play()
