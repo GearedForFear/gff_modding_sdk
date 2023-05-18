27ea33803f07c28d2727576c91eb58ec2ad12dac
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:30:40 +0200

    Update rigid.gd

--- base_game/scripts/cameras_&_ui/buttons/rigid.gd
+++ base_game/scripts/cameras_&_ui/buttons/rigid.gd
@@ -3,10 +3,14 @@ extends Button
 
 export var max_rigid_bodies: int = 100
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").max_rigid_bodies \
-			= max_rigid_bodies
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	root_control.get_node("SettingsManager").max_rigid_bodies = max_rigid_bodies
+	config.set_value("graphics", "max_rigid_bodies", max_rigid_bodies)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/MaxRigidBodies"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
