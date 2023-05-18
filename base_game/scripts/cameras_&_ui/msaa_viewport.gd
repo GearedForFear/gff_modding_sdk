50b1223c47dd5215bdab384a3f9ba4126335f740
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 20:59:16 +0200

    Update and rename msaa_viewport.gd to sub_viewport.gd

--- base_game/scripts/cameras_&_ui/msaa_viewport.gd
+++
@@ -1,5 +0,0 @@
-extends Viewport
-
-
-func _ready():
-	msaa = get_node("/root/RootControl/SettingsManager").msaa
---
+++ base_game/scripts/cameras_&_ui/sub_viewport.gd
@@ -0,0 +1,14 @@
+extends Viewport
+
+
+func _ready():
+	msaa = get_node("/root/RootControl/SettingsManager").msaa
+
+
+func stop():
+	render_target_update_mode = UPDATE_DISABLED
+	$Timer.start()
+
+
+func _on_Timer_timeout():
+	render_target_update_mode = UPDATE_WHEN_VISIBLE
