ab68260873f63c5dec63088e31763431f2181516
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:35:24 +0200

    Update window_modes.gd

--- base_game/scripts/cameras_&_ui/buttons/window_modes.gd
+++ base_game/scripts/cameras_&_ui/buttons/window_modes.gd
@@ -4,10 +4,16 @@ extends Button
 export var borderless: bool = false
 export var fullscreen: bool = false
 
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
+
 
 func _pressed():
 	OS.window_borderless = borderless
 	OS.window_fullscreen = fullscreen
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	config.set_value("graphics", "borderless", borderless)
+	config.set_value("graphics", "fullscreen", fullscreen)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/WindowModes"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
