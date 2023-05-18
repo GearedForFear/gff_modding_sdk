ab8a8b392435f3ce728921875ed8876f077dd390
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-18 21:33:33 +0200

    Update view_distance.gd

--- base_game/scripts/cameras_&_ui/buttons/view_distance.gd
+++ base_game/scripts/cameras_&_ui/buttons/view_distance.gd
@@ -1,12 +1,16 @@
 extends Button
 
 
-export var distance: int = 1000.0
+export var distance: float = 1000.0
+
+onready var root_control: Control = get_node("/root/RootControl")
+onready var config: ConfigFile = root_control.config
 
 
 func _pressed():
-	get_node("/root/RootControl/SettingsManager").view_distance \
-			= distance
-	get_node("/root/RootControl").switch_buttons(get_parent(), \
+	root_control.get_node("SettingsManager").view_distance = distance
+	config.set_value("graphics", "view_distance", distance)
+	config.save("user://config.cfg")
+	root_control.switch_buttons(get_parent(), \
 			get_node("../../GraphicsButtons/ViewDistance"))
-	get_node("/root/RootControl/ButtonPressAudio").play()
+	root_control.get_node("ButtonPressAudio").play()
