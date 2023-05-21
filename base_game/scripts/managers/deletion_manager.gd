84d19648fc7d4bf32313753273b2de04e8e6770a
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-05-21 23:42:14 +0200

    Update deletion_manager.gd

--- base_game/scripts/managers/deletion_manager.gd
+++ base_game/scripts/managers/deletion_manager.gd
@@ -4,12 +4,13 @@ extends Node
 var to_be_deleted: Array
 var gameplay_rigid_bodies: Array
 var other_rigid_bodies: Array
+var delete: bool = false
 
 onready var max_rigid_bodies: int \
 		= get_node("/root/RootControl/SettingsManager").max_rigid_bodies
 
 
-func _physics_process(_delta):
+func _process(delta):
 	var total_rigid_bodies: int = gameplay_rigid_bodies.size() \
 			+ other_rigid_bodies.size()
 	if total_rigid_bodies > max_rigid_bodies:
@@ -22,9 +23,16 @@ func _physics_process(_delta):
 			other_rigid_bodies.remove(n)
 			to_be_deleted.append(delete_body)
 	
-	for n in to_be_deleted.size() / 30 + 1:
-		if not to_be_deleted.empty():
-			var next_deletion: Node = to_be_deleted[to_be_deleted.size() - 1]
-			if(is_instance_valid(next_deletion)):
-				next_deletion.queue_free()
-			to_be_deleted.remove(to_be_deleted.size() - 1)
+	if delete or delta > 0.016:
+		for n in to_be_deleted.size() / 30 + 1:
+			if not to_be_deleted.empty():
+				var next_deletion: Node = to_be_deleted[to_be_deleted.size() - 1]
+				if(is_instance_valid(next_deletion)):
+					next_deletion.queue_free()
+				to_be_deleted.remove(to_be_deleted.size() - 1)
+	
+	delete = true
+
+
+func _physics_process(_delta):
+	delete = false
