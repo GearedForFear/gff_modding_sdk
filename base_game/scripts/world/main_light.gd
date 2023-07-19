fb10a3f7b0e35b9c6bfb82c235da41976ed6ec9a
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-07-19 15:18:35 +0200

    Update and rename directional_light.gd to main_light.gd

--- base_game/scripts/world/main_light.gd
+++ base_game/scripts/world/main_light.gd
@@ -2,5 +2,6 @@ extends DirectionalLight
 
 
 func _ready():
+	directional_shadow_normal_bias = OS.window_size.x / OS.window_size.y / 2
 	directional_shadow_max_distance \
 			= get_node("/root/RootControl/SettingsManager").shadow_distance
