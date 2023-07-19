9542b2014030b5e6b8c697dd16a6c8daf7190fb8
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-07-19 15:09:23 +0200

    Update wheel_mesh.gd

--- base_game/scripts/vehicles/wheel_mesh.gd
+++ base_game/scripts/vehicles/wheel_mesh.gd
@@ -2,12 +2,12 @@ extends ShadowSetter
 
 
 var interpolated_parent: bool = true
-var on_screen: bool = true
 var wait: bool = false
+var visible_on: Array
 
 
 func _process(_delta):
-	if interpolated_parent and on_screen:
+	if interpolated_parent and not visible_on.empty():
 		global_translation = get_parent().get_global_transform_interpolated().origin
 
 
@@ -24,9 +24,9 @@ func _notification(what):
 		wait = true
 
 
-func _on_VisibilityNotifier_screen_entered():
-	on_screen = true
+func _on_VisibilityNotifier_camera_entered(camera):
+	visible_on.append(camera)
 
 
-func _on_VisibilityNotifier_screen_exited():
-	on_screen = false
+func _on_VisibilityNotifier_camera_exited(camera):
+	visible_on.erase(camera)
