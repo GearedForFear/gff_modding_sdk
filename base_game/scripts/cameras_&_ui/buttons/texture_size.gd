924d84445c7eb972feed4b894620e552cad9cc21
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-07-19 15:19:53 +0200

    Update and rename shadow_distance.gd to texture_size.gd

--- base_game/scripts/cameras_&_ui/buttons/texture_size.gd
+++ base_game/scripts/cameras_&_ui/buttons/texture_size.gd
@@ -1,8 +1,8 @@
 extends Button
 
 
-export var shadow_distance: float = 200.0
-export var shadow_quality: int = 4096
+export var shadow_distance: float = 125.0
+export var max_texture_size: int = 4096
 
 onready var root_control: Control = get_node("/root/RootControl")
 onready var config: ConfigFile = root_control.config
@@ -10,11 +10,12 @@ onready var config: ConfigFile = root_control.config
 
 func _pressed():
 	root_control.get_node("SettingsManager").shadow_distance = shadow_distance
+	root_control.get_node("SettingsManager").max_texture_size = max_texture_size
 	ProjectSettings.set_setting("rendering/quality/directional_shadow/size", \
-			shadow_quality)
+			max_texture_size)
 	config.set_value("graphics", "shadow_distance", shadow_distance)
-	config.set_value("graphics", "shadow_quality", shadow_quality)
+	config.set_value("graphics", "max_texture_size", max_texture_size)
 	config.save("user://config.cfg")
 	root_control.switch_buttons(get_parent(), \
-			get_node("../../GraphicsButtons/ShadowDistance"))
+			get_node("../../GraphicsButtons/TextureSize"))
 	root_control.get_node("ButtonPressAudio").play()
