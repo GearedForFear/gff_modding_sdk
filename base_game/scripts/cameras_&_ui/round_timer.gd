79e5783aaa3d7f4c345412e6af22dd1b6cf135c8
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-08-25 23:19:20 +0200

    Update round_timer.gd

--- base_game/scripts/cameras_&_ui/round_timer.gd
+++ base_game/scripts/cameras_&_ui/round_timer.gd
@@ -6,10 +6,10 @@ var track: Spatial
 
 func _ready():
 	track = get_node("../../../../..").track
+	$Above.text = "Stage " \
+			+ String(6 - get_node("/root/RootControl").next_tracks.size())
 
 
 func _physics_process(_delta):
-	$Above.text = "Stage " \
-			+ String(6 - get_node("/root/RootControl").next_tracks.size())
 	$TimeLeft.text = \
 			String(int(track.get_node("GameplayManager/Timer").time_left))
