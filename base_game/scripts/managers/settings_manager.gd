bea1290f387f9e9a74d7df5e710f183a54b8091e
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-10-31 00:21:41 +0100

    Update settings_manager.gd

--- base_game/scripts/managers/settings_manager.gd
+++ base_game/scripts/managers/settings_manager.gd
@@ -7,8 +7,9 @@ var view_distance: float = 1000.0
 var rear_view_distance: float = 200.0
 var field_of_view: float = 75.0
 var shadow_casters: int = 3
-var shadow_distance: float = 200.0
+var shadow_distance: float = 125.0
 var max_texture_size: int = 4096
+var reflections: int = 0
 var max_rigid_bodies: int = 100
 
 var split_screen_divisor: int = 1
