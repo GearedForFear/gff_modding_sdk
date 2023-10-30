49b64cda3e86c7fb602944da52362d4114691635
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2023-10-31 00:23:37 +0100

    Update pool_body.gd

--- base_game/scripts/world/pool_body.gd
+++ base_game/scripts/world/pool_body.gd
@@ -7,12 +7,16 @@ onready var deletion_manager: Node = get_node("../../../DeletionManager")
 var pool: Array
 
 
+func _physics_process(_delta):
+	if global_translation.y <= -32:
+		stop()
+
+
 func start(global_transform: Transform):
 	self.global_transform = global_transform
 	linear_velocity = Vector3.ZERO
 	angular_velocity = Vector3.ZERO
-	collision_layer = 4
-	collision_mask = 6
+	collision_mask = 22
 	set_physics_process(true)
 	set_process(true)
 	show()
@@ -21,7 +25,6 @@ func start(global_transform: Transform):
 
 
 func stop():
-	collision_layer = 0
 	collision_mask = 0
 	set_physics_process(false)
 	set_process(false)
