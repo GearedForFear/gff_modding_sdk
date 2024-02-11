4c9546ddad7cf855ed5a8f668d36c942ae66dc55
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2024-02-11 18:54:30 +0100

    Update settings.gd

--- base_game/scripts/cameras_&_ui/buttons/settings.gd
+++ base_game/scripts/cameras_&_ui/buttons/settings.gd
@@ -2,6 +2,8 @@ extends Button
 
 
 func _pressed():
+	get_node("/root/RootControl/Names").hide()
+	get_node("/root/RootControl/Scores").hide()
 	get_node("/root/RootControl/CurrentSettings").show()
 	get_node("/root/RootControl").switch_buttons(get_parent(), \
 			get_node("../../SettingsButtons/Graphics"))
