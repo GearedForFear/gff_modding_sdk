f9f80da3a40a721f09db07b35c59a4187fd02caa
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2024-02-11 18:57:51 +0100

    Update grenade.gd

--- base_game/scripts/weapon_compontents/grenade.gd
+++ base_game/scripts/weapon_compontents/grenade.gd
@@ -1,56 +1,5 @@
-extends Area
+extends ArcProjectile
 
 
-const Explosion: PackedScene = preload("res://scenes/destruction/explosion.tscn")
-
-var damage: float
-var reward: int
-var burn: float
-var shooter: CombatVehicle
-var deletion_manager: Node
-
-var interpolation_weight: float = 0.01
-
-
-func _ready():
-	set_as_toplevel(true)
-	$Lifetime.wait_time *= 60.0 \
-			/ ProjectSettings.get_setting("physics/common/physics_fps")
-	$Lifetime.start()
-
-
-func _physics_process(_delta):
-	translation += transform.basis.z * 0.7
-	global_transform = global_transform.interpolate_with(global_transform.\
-			looking_at(global_translation - global_transform.basis.z \
-			+ Vector3.UP, Vector3.UP), interpolation_weight)
-	interpolation_weight *= 1.02
-
-
-func _on_Lifetime_timeout():
-	set_process(false)
-	set_physics_process(false)
-	hide()
-	deletion_manager.to_be_deleted.append(self)
-
-
-func _on_Area_body_entered(body):
-	if body != shooter:
-		if true:
-			$RayCast.force_raycast_update()
-			var new_explosion: Area = Explosion.instance()
-			new_explosion.damage = damage
-			new_explosion.reward = reward
-			new_explosion.burn = burn
-			new_explosion.shooter = shooter
-			new_explosion.deletion_manager = deletion_manager
-			body.add_child(new_explosion)
-			new_explosion.global_transform = global_transform
-			new_explosion.global_transform.origin \
-					= $RayCast.get_collision_point()
-			if $RayCast.get_collision_point() == Vector3.ZERO:
-				new_explosion.global_transform.origin = global_transform.origin
-		set_process(false)
-		set_physics_process(false)
-		hide()
-		deletion_manager.to_be_deleted.append(self)
+func make_available():
+	pools.grenades_available.append(self)
