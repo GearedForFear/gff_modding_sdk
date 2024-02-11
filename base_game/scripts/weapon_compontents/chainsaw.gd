bbbd5f284da435ce922906d0f433569b2d48f786
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2024-02-11 18:57:30 +0100

    Update chainsaw.gd

--- base_game/scripts/weapon_compontents/chainsaw.gd
+++ base_game/scripts/weapon_compontents/chainsaw.gd
@@ -1,63 +1,30 @@
-extends Area
+extends ArcProjectile
 
 
-var damage: float
-var reward: int
-var burn: float
-var shooter: CombatVehicle
-var deletion_manager: Node
-var pools: Node
-
-var interpolation_weight: float = 0.05
+func start(global_transform: Transform, damage: float, reward: int, \
+		burn: float, shooter: CombatVehicle):
+	$AudioStreamPlayer3D.play()
+	.start(global_transform, damage, reward, burn, shooter)
 
 
-func _ready():
-	set_as_toplevel(true)
-	$Lifetime.wait_time *= 60.0 \
-			/ ProjectSettings.get_setting("physics/common/physics_fps")
-	$Lifetime.start()
+func collide(var body: PhysicsBody):
+	$AudioStreamPlayer3D.stop()
+	impact_audio_playing = true
+	if body.is_in_group("combat_vehicle"):
+		$ImpactVehicle.play()
+	else:
+		$ImpactGround.play()
 
 
-func _physics_process(_delta):
-	translation += transform.basis.z * 0.3
-	global_transform = global_transform.interpolate_with(global_transform.\
-			looking_at(global_translation - global_transform.basis.z \
-			+ Vector3.UP, Vector3.UP), interpolation_weight)
-	interpolation_weight *= 1.01
-
-
-func delete():
-	set_process(false)
-	set_physics_process(false)
-	hide()
-	deletion_manager.to_be_deleted.append(self)
-
-
-func _on_Area_body_entered(body):
-	if body != shooter:
-		if body.is_in_group("combat_vehicle"):
-			rotation.z = randi() / TAU
-			pools.get_sparks().start(global_transform)
-			
-			var payout: int = body.damage(damage, reward, burn, shooter)
-			if payout > 0:
-				pools.get_money().start(global_transform, shooter, body, payout)
-			$ImpactVehicle.play()
-		else:
-			$ImpactGround.play()
-		$Lifetime.stop()
-		set_process(false)
-		set_physics_process(false)
-		hide()
-
-
-func _on_Lifetime_timeout():
-	delete()
+func make_available():
+	pools.chainsaws_available.append(self)
 
 
 func _on_ImpactVehicle_finished():
-	delete()
+	impact_audio_playing = false
+	try_make_available()
 
 
 func _on_ImpactGround_finished():
-	delete()
+	impact_audio_playing = false
+	try_make_available()
