2ee056e6f0f534372f54d155f9a2d7de7a2a5e85
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2024-04-22 00:11:26 +0200

    Update environment.gd

--- base_game/scripts/world/environment.gd
+++ base_game/scripts/world/environment.gd
@@ -4,5 +4,7 @@ extends WorldEnvironment
 func _ready():
 	var max_steps: int = \
 			get_node("/root/RootControl/SettingsManager").reflections
-	environment.ss_reflections_max_steps = max_steps
+	environment.ss_reflections_max_steps = min(OS.window_size.x \
+			/ get_node("/root/RootControl/SettingsManager").resolution / 4, \
+			max_steps)
 	environment.ss_reflections_enabled = max_steps != 0
