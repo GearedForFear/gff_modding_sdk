781e3ee9c35365f5782f215978b13c4d263f7dc0
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2026-02-27 19:44:11 +0100

    Hud redesign

--- base_game/scripts/cameras_&_ui/resizing_control_font.gd
+++ base_game/scripts/cameras_&_ui/resizing_control_font.gd
@@ -6,14 +6,14 @@ export var base_font_size: float = 9.0
 
 func _ready():
 	var f: DynamicFont = get_font("font")
-	f.size = int(base_font_size * clamp(OS.window_size.y, 0, OS.window_size.x \
-			* 9 / 16) / 360 / get_node("/root/RootControl/SettingsManager")\
-			.split_screen_divisor)
-	f.size /= get_node("/root/RootControl/SettingsManager").resolution
-
-	if f.outline_size != 0:
-		f.outline_size = int(clamp(OS.window_size.y, 0, OS.window_size.x * 9 \
-				/ 16) / 360 / get_node("/root/RootControl/SettingsManager")\
-				.split_screen_divisor)
-		f.outline_size \
-				/= get_node("/root/RootControl/SettingsManager").resolution
+#	f.size = int(base_font_size * clamp(OS.window_size.y, 0, OS.window_size.x \
+#			* 9 / 16) / 360 / get_node("/root/RootControl/SettingsManager")\
+#			.split_screen_divisor)
+#	f.size /= get_node("/root/RootControl/SettingsManager").resolution
+#
+#	if f.outline_size != 0:
+#		f.outline_size = int(clamp(OS.window_size.y, 0, OS.window_size.x * 9 \
+#				/ 16) / 360 / get_node("/root/RootControl/SettingsManager")\
+#				.split_screen_divisor)
+#		f.outline_size \
+#				/= get_node("/root/RootControl/SettingsManager").resolution
