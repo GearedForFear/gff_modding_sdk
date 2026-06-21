d85b903e770ebb46e3d0f612460dcd6e15a6a3aa
GearedForFear <113252390+GearedForFear@users.noreply.github.com>
2026-06-21 22:48:27 +0200

    Update vehicle customization

--- base_game/scripts/weapon_compontents/fancy_gun.gd
+++ base_game/scripts/weapon_compontents/fancy_gun.gd
@@ -6,4 +6,6 @@ var projectile_material: ShaderMaterial
 
 func shoot(shooter: CombatVehicle) -> Projectile:
 	var return_value: Projectile = .shoot(shooter)
+	return_value.get_node("MeshInstance").set_surface_material(0,
+			projectile_material)
 	return return_value
